e0f6992f5b791c67c159473f75c5115f5f2e11b8
Ruwini98 <91079502+Ruwini98@users.noreply.github.com>
2022-01-22 12:32:43 +0530

    Add files via upload

---
+++ datasearch/divider.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Search data | M cloud</title>
+    <link rel="icon" href="../images/logo.png">
+</head>
+
+<body>
+
+    <a href="..\user\patient\patientLoginForum.php"><b>View My Medical Data</b></a>
+    <a href="..\user\doctor\doctorLoginForum.php"><b>View Patient Medical Data</b></a>
+
+    <script src="app.js"></script>
+</body>
+
+</html>
